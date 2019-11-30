0b85f2274eef0f7bf08e444eb1b2598d0689d95d
Ratul Sarkar <ratul96@yahoo.in>
2019-11-30 23:08:29 +0000

    New PBR Testing :wq

--- PGG_OBJ_Loader/Material.cpp
+++ PGG_OBJ_Loader/Material.cpp
@@ -332,7 +332,7 @@ void Material::Apply()
 	glUniform3f(glGetUniformLocation(_shaderProgram, "lightColors[3]"), 300.0f, 300.0f, 300.0f);
 
 	glUniform3f(glGetUniformLocation(_shaderProgram, "viewPos"), 0.0f, 0.0f, 0.0f);
-	glUniform1f(glGetUniformLocation(_shaderProgram, "metallic"), 1.0f);
+	glUniform1f(glGetUniformLocation(_shaderProgram, "metallic"), 0.6f);
 	glUniform1f(glGetUniformLocation(_shaderProgram, "roughness"), 0.0f);
 
 	
